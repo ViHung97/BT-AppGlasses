efcdabef7967419acad2fe7b59a06569c720fbd1
ViHung97 <tranvihung236.work@gmail.com>
2023-06-27 00:09:03 +0700

    update src

--- src/Glasses/index.js
+++ src/Glasses/index.js
@@ -1,44 +1,48 @@
 import React, { Component } from "react";
-import dataGlasses from "./dataGlasses.json"
+import dataGlasses from "./dataGlasses.json";
 
 export default class TryGlasses extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      CurrentGlasses: [{
-        id: 1,
-        price: 30,
-        name: "GUCCI G8850U",
-        url: "./glassesImage/v1.png",
-        desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
-      }],
+      currentGlasses: [
+        {
+          id: 1,
+          price: 30,
+          name: "GUCCI G8850U",
+          url: "./glassesImage/v1.png",
+          desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+        },
+      ],
     };
   }
 
-  currentGlasses=(id, url)=>{
-    console.log(id, url)
-    let CurrentGlasses = [...this.state.CurrentGlasses]
-    if (CurrentGlasses.id === id) {
-      CurrentGlasses.url = url
-    }
-    this.setState({
-      CurrentGlasses
-    })
-  }
+  currentGlasses = (item) => {
+    return this.setState({
+      currentGlasses: item,
+    });
+  };
 
-  renderListGalasses=()=>{
-    const listGlasses = dataGlasses
-    console.log(listGlasses)
-    return listGlasses.map((item)=>{
-      return <img onClick={()=>{this.currentGlasses(item.id, item.url)}} key={item.id} src={item.url} alt="" />
-    })
-  }
-  
+  renderListGalasses = () => {
+    const listGlasses = dataGlasses;
+    return listGlasses.map((item) => {
+      return (
+        <img
+          onClick={() => {
+            this.currentGlasses(item);
+          }}
+          key={item.id}
+          src={item.url}
+          alt=""
+        />
+      );
+    });
+  };
 
   render() {
+    const { currentGlasses } = this.state;
     return (
       <div className="app">
-
         <div>
           <h1 className="title">TRY GLASSES APP ONLINE</h1>
         </div>
@@ -47,11 +51,9 @@ export default class TryGlasses extends Component {
             <img className="model" src="./glassesImage/model.jpg" alt="" />
             <img className="model" src="./glassesImage/model.jpg" alt="" />
           </div>
-          <img className="currentGlasses" src="./glassesImage/v1.png" alt="" />
+          <img className="currentGlasses" src={currentGlasses.url} alt="" />
         </div>
-        <div className="listGlasses">
-          {this.renderListGalasses()}
-        </div>
+        <div className="listGlasses">{this.renderListGalasses()}</div>
       </div>
     );
   }
